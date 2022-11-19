fb8f2ecf176b7e5a9d695645eae390d9740e61db
Estefano23444 <105741695+Estefano23444@users.noreply.github.com>
2022-11-19 18:42:04 -0500

    Cambios Aplicados por Estefano Proaño
    Solo se coloco bien el nombre

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Estefano Proaño");
+        System.out.println("Hola Estefano");
     }
 }
