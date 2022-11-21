08d8dfefba71c83ea4d716fc9c6177ba1c421fe5
AnibalRocha <105742218+AnibalRocha@users.noreply.github.com>
2022-11-21 18:52:09 -0500

    Update App.java
    agregado por Luis Rocha

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Estefano");
         System.out.println("Hola Isaac");
+        System.out.println("Hola Luis");
     }
 }
